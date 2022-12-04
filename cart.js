d8cf60ea57321c112dcdcab31d091b2569062871
KShengYao <s10247882@connect.np.edu.sg>
2022-12-04 23:03:11 +0800

    updated

--- cart.js
+++ cart.js
@@ -139,36 +139,176 @@ if(document.getElementById("cartInfo7").innerHTML == "")
         total = (localStorage.getItem("Quantity7") * localStorage.getItem("Price7")) + total;
     }
 }  
-    
 
 const totalamt = document.getElementById("totalAmount");
 totalamt.innerHTML = "SGD$" + total;
 
-
 function ClearAll(){
-    if(document.getElementById("Delivery").elements["Delivery"].value == "Express Delivery")
+    empty1 = true;
+    empty2 =true;
+    empty3 =true;
+    empty4 =true;
+    empty5 =true;
+    empty6 =true;
+    empty7 =true;
+    empty8 = true;
+
+    if(total == 0)
     {
-        total = total + 5 
+        alert("CART EMPTY");
     }
+    else
+    {
+        if(document.getElementById("CheckoutName").value == "")
+        {
+            document.getElementById("valiCheckoutName").innerHTML = "Please enter a something";
+        }
+        else
+        {
+            document.getElementById("valiCheckoutName").innerHTML = "";
+            empty1 = false;
+        }
 
-    alert("Name: " + document.getElementById("CheckoutName").value+"\n"+
-    "ContactNo: " + document.getElementById("checkoutContactNo").value +"\n"+
-    "ContactNo: " + document.getElementById("checkoutContactNo").value +"\n"+
-    "Country: " + document.getElementById("Country").value +"\n"+
-    "Address: " + document.getElementById("address").value +"("+ document.getElementById("postal").value + ")" + "\n"+
-    "Payment: " + document.getElementById("CreditCard").elements["cc"].value + " (Approved)" + "\n"+
-    "Total Amount payed: " +"SGD$"+ total + "\n"+"\n"+
-    "Thanks for shopping!! Hope to see u again!!!")
 
-    document.getElementById("CheckoutName").value = "";
-    document.getElementById("checkoutContactNo").value = "";
-    document.getElementById("checkoutContactNo").value = "";
-    document.getElementById("postal").value = "";
-    document.getElementById("address").value = "";
+        if(document.getElementById("checkoutContactNo").value == "")
+        {
+            document.getElementById("valicheckoutContactNo").innerHTML = "Please enter a something";
+        }
+        else
+        {
+            document.getElementById("valicheckoutContactNo").innerHTML = "";
+            if(isNaN(document.getElementById("checkoutContactNo").value))
+            {
+                document.getElementById("valicheckoutContactNo").innerHTML = "Please enter a Numeric Number";
+            }
+            else
+            {
+                document.getElementById("valicheckoutContactNo").innerHTML = "";
+                empty2 = false;
+            }
+            
+        }
 
-    document.getElementById("PaymentName").value = "";
-    document.getElementById("PaymentCArd").value = "";
-    document.getElementById("PaymentPostal").value = "";
-    document.getElementById("PaymentAddress").value = "";
-    ClearDataBase();
+        if(document.getElementById("postal").value == "" )
+        {
+            document.getElementById("valipostal").innerHTML = "Please enter a something";
+        }
+        else
+        {
+            document.getElementById("valipostal").innerHTML = "";
+            if(isNaN(document.getElementById("postal").value))
+            {
+                document.getElementById("valipostal").innerHTML = "Please enter a Numeric Number";
+            }
+            else
+            {
+                document.getElementById("valipostal").innerHTML = "";
+                if(document.getElementById("postal").value.length != 6)
+                {
+                    document.getElementById("valipostal").innerHTML = "Number length up to 6";
+                }
+                else
+                {
+                    document.getElementById("valipostal").innerHTML = "";
+                    empty3 = false;
+                }
+            }
+            
+        }
+
+        if(document.getElementById("address").value == "" )
+        {
+            document.getElementById("valiaddress").innerHTML = "Please enter a something";
+        }
+        else
+        {   
+            document.getElementById("valiaddress").innerHTML = "";
+            empty4 = false;
+        }
+
+        if(document.getElementById("Delivery").elements["Delivery"].value == "" )
+        {
+            document.getElementById("validel").innerHTML = "Please select something";
+        }
+        else
+        {
+            document.getElementById("validel").innerHTML = "";
+            empty5 = false;
+        }
+
+        if(document.getElementById("CreditCard").elements["cc"].value == "" )
+        {
+            document.getElementById("ValiCreditCard").innerHTML = "Please select something";
+        }
+        else
+        {
+            document.getElementById("ValiCreditCard").innerHTML = "";
+            empty6 = false;
+        }
+
+        if(document.getElementById("PaymentCArd").value == "")
+        {
+            document.getElementById("valiPaymentCArd").innerHTML = "Please select something";
+        }
+        else
+        {
+            document.getElementById("valiPaymentCArd").innerHTML = "";
+            if(isNaN(document.getElementById("PaymentCArd").value))
+            {
+                document.getElementById("valiPaymentCArd").innerHTML = "Please enter a Numeric Number";
+            }
+            else
+            {
+                document.getElementById("valiPaymentCArd").innerHTML = "";
+                if(document.getElementById("PaymentCArd").value.length != 16)
+                {
+                    document.getElementById("valiPaymentCArd").innerHTML = "Number length up to 16";
+                }
+                else
+                {
+                    document.getElementById("valiPaymentCArd").innerHTML = "";
+                    empty7 = false;
+                }
+            }
+        }
+
+        if(document.getElementById("PaymentName").value == "")
+        {
+            document.getElementById("valiPaymentName").innerHTML = "Please select something";
+        }
+        else
+        {
+            document.getElementById("valiPaymentName").innerHTML = "";
+            empty8 = false;
+        }
+
+
+        if(empty1 != true && empty2 != true && empty3 != true && empty4 != true && empty5 != true && empty6 != true && empty7 != true && empty8 != true)
+        {
+            if(document.getElementById("Delivery").elements["Delivery"].value == "Express Delivery")
+            {
+                total = total + 5 
+            }
+            alert("Name: " + document.getElementById("CheckoutName").value+"\n"+
+            "ContactNo: " + document.getElementById("checkoutContactNo").value +"\n"+
+            "ContactNo: " + document.getElementById("checkoutContactNo").value +"\n"+
+            "Country: " + document.getElementById("Country").value +"\n"+
+            "Address: " + document.getElementById("address").value +"("+ document.getElementById("postal").value + ")" + "\n"+
+            "Payment: " + document.getElementById("CreditCard").elements["cc"].value + " (Approved)" + "\n"+
+            "Total Amount payed: " +"SGD$"+ total + "\n"+"\n"+
+            "Thanks for shopping!! Hope to see u again!!!")
+                
+            document.getElementById("CheckoutName").value = "";
+            document.getElementById("checkoutContactNo").value = "";
+            document.getElementById("checkoutContactNo").value = "";
+            document.getElementById("postal").value = "";
+            document.getElementById("address").value = "";
+                
+            document.getElementById("PaymentName").value = "";
+            document.getElementById("PaymentCArd").value = "";
+            document.getElementById("PaymentPostal").value = "";
+            document.getElementById("PaymentAddress").value = "";
+            ClearDataBase();
+        }
+    }
 }
