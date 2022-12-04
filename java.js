a0c6c326f3f674a0a4d21e01044766765f25205f
KShengYao <s10247882@connect.np.edu.sg>
2022-12-05 00:59:59 +0800

    Videos Updated because of naming

--- java.js
+++ java.js
@@ -19,7 +19,7 @@ function ClickingVideo() {
     var description = document.getElementById("description")
     var ReleaseDate = document.getElementById("VideoReleaseJs")
 
-    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE - インフェルノ（Inferno).mp4'); 
+    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE Inferno.mp4'); 
     title.innerHTML = "Mrs. GREEN APPLE - インフェルノ（Inferno)"
     description.innerHTML = "Description: Inferno (インフェルノ or Inferuno in Japanese) is a song released by the Japanese rock band Mrs. GREEN APPLE. It was released as a part of the group’s album, Attitude in 2019.It was used as the opening theme for Season 1 of the anime Fire Force (Enn Enn no Shouboutai or 炎炎ノ消防隊 in Japanese)."
     ReleaseDate.innerHTML = "02.08.2019"
@@ -37,7 +37,7 @@ function ClickingVideo1() {
     var ReleaseDate = document.getElementById("VideoReleaseJs")
     
 
-    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE「Soranji」Official Music Video.mp4'); 
+    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE Soranji Official Music Video.mp4'); 
     title.innerHTML = "Mrs. GREEN APPLE「Soranji」Official Music Video"
     description.innerHTML = ""
     ReleaseDate.innerHTML = "02.11.2022"
@@ -54,7 +54,7 @@ function ClickingVideo2() {
     var ReleaseDate = document.getElementById("VideoReleaseJs")
     
 
-    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE「フロリジナル」Official Music Video.mp4'); 
+    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE Floriginal Official Music Video.mp4'); 
     title.innerHTML = "Mrs. GREEN APPLE「フロリジナル」Official Music Video"
     description.innerHTML = "PARFA TUNE is a collaboration project in which FITS invites various artists to create unique perfumes along with original songs. Using a traditional method called (incense scale) invented in England in the 19th century, sounds are expressed as scents. The incense scale is a musical scale applied to 46 types of fragrances. Highly volatile scents are assigned to high notes, and low notes are assigned to low notes. It was invented in the 19th century by English perfumer Septimus Pies, who was also a chemist."
     ReleaseDate.innerHTML = "07.11.2022"
@@ -71,7 +71,7 @@ function ClickingVideo3() {
     var ReleaseDate = document.getElementById("VideoReleaseJs")
     
 
-    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE「ダンスホール」Official Music Video.mp4'); 
+    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE Dancehall.mp4'); 
     title.innerHTML = "Mrs. GREEN APPLE「ダンスホール」Official Music Video"
     description.innerHTML = ""
     ReleaseDate.innerHTML = "28.06.2022"
@@ -88,7 +88,7 @@ function ClickingVideo4() {
     var ReleaseDate = document.getElementById("VideoReleaseJs")
     
 
-    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE「ニュー・マイ・ノーマル」Official Music Video.mp4'); 
+    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE NEW MY NORMAL Official Music Video.mp4'); 
     title.innerHTML = "Mrs. GREEN APPLE「ニュー・マイ・ノーマル」Official Music Video.mp4"
     description.innerHTML = ""
     ReleaseDate.innerHTML = "18.03.2022"
@@ -105,7 +105,7 @@ function ClickingVideo5() {
     var ReleaseDate = document.getElementById("VideoReleaseJs")
     
 
-    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE – 「青と夏」 Official Lyric Video.mp4'); 
+    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE Ao to Natsu Natsu.mp4'); 
     title.innerHTML = "Mrs. GREEN APPLE – 「青と夏」 Official Lyric Video"
     description.innerHTML = "Mrs. GREEN APPLE has released a lyric video that cuts out the Japanese summer scenery in commemoration of the number of streaming views of Phase 1's representative summer song (Ao to Natsu) surpassing 100 million views!"
     ReleaseDate.innerHTML = "18.03.2022"
@@ -122,7 +122,7 @@ function ClickingVideo6() {
     var ReleaseDate = document.getElementById("VideoReleaseJs")
     
 
-    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE - Theater.mp4'); 
+    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE Theater.mp4'); 
     title.innerHTML = "Mrs. GREEN APPLE - Theater"
     description.innerHTML = "Phase 1, thank you so much everyone! I am grateful for the many encounters I have had. Phase 2 is next. See you again!!"
     ReleaseDate.innerHTML = "08.07.2020"
@@ -138,7 +138,7 @@ function ClickingVideo7() {
     var ReleaseDate = document.getElementById("VideoReleaseJs")
     
 
-    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE-「スターダム」Official Lyric Video.mp4'); 
+    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE Stardom.mp4'); 
     title.innerHTML = "Mrs. GREEN APPLE-「スターダム」Official Lyric Video"
     description.innerHTML = "Released on July 8, 2020, released the lyric video of 'Stardom' from the best album '5', which will be the conclusion of Mrs. GREEN APPLE Phase 1!!"
     ReleaseDate.innerHTML = "24.06.2020"
@@ -154,7 +154,7 @@ function ClickingVideo8() {
     var ReleaseDate = document.getElementById("VideoReleaseJs")
     
 
-    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE- WanteD! WanteD! (Side Story ver.).mp4'); 
+    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE WanteD WanteD Side Story ver.mp4'); 
     title.innerHTML = "Mrs. GREEN APPLE- WanteD! WanteD! (Side Story ver.)"
     description.innerHTML = 'The music video for "WanteD! WanteD!" (Side Story ver.) from the best album "5", which will be released on Wednesday, July 8th, means "Phase 1 is complete"!!It is another story of the 2017 "WanteD! WanteD!" music video. Special thanks to Yurina Hirate'
     ReleaseDate.innerHTML = "03.07.2020"
@@ -170,7 +170,7 @@ function ClickingVideo9() {
     var ReleaseDate = document.getElementById("VideoReleaseJs")
     
 
-    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE – アボイドノート.mp4'); 
+    changingVideo.setAttribute('src', 'video/Mrs. GREEN APPLE Avoid Note.mp4'); 
     title.innerHTML = "Mrs. GREEN APPLE – アボイドノート"
     description.innerHTML = 'Released on July 8, 2020, the music video for the new song "Avoid Note" from the best album "5", which will be the conclusion of Mrs. GREEN APPLE Phase 1, has been released!!'
     ReleaseDate.innerHTML = "27.05.2020"
